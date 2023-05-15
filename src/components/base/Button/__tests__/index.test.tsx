efa0e55dd1ac2b5e42a89f2a0498c7840b3c226c
solehudin5699 <solehudin5699@gmail.com>
2023-05-16 00:43:07 +0700

    chore: update vites config, example test, .gitignore

--- src/components/base/Button/__tests__/index.test.tsx
+++ src/components/base/Button/__tests__/index.test.tsx
@@ -1,9 +1,8 @@
-import { expect } from 'vitest';
 import { render, screen } from '../../../../test-utils';
 import Button from '@/components/base/Button';
 
-describe('first', () => {
-  test('a', () => {
+describe('Button', () => {
+  test('should render correctly', () => {
     render(<Button>Test</Button>);
 
     expect(screen.getByText('Test')).toBeInTheDocument();
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
+import { defineConfig, mergeConfig, defaultExclude, coverageConfigDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
 //SOURCE: https://vitest.dev/config/
@@ -8,11 +8,7 @@ export default mergeConfig(
     test: {
       include: ['src/**/*.test.{ts,tsx}'],
       exclude: [
-        '**/node_modules/**',
-        '**/dist/**',
-        '**/cypress/**',
-        '**/.{idea,git,cache,output,temp}/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        ...defaultExclude,
         'src/assets/**',
         'src/configs/**',
         'src/context/**',
@@ -26,18 +22,7 @@ export default mergeConfig(
         enabled: false,
         include: ['src/**/*.{ts,tsx}'],
         exclude: [
-          'coverage/**',
-          'dist/**',
-          'packages/*/test{,s}/**',
-          '**/*.d.ts',
-          'cypress/**',
-          'test{,s}/**',
-          'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
-          '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
-          '**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
-          '**/__tests__/**',
-          '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-          '**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
+          ...coverageConfigDefaults.exclude,
           'src/assets/**',
           'src/configs/**',
           'src/context/**',
